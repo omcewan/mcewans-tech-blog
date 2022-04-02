1f591faee01bf09a1f4e2d09e581f3e3a219cc71
omcewan <orlandomcewan231@gmail.com>
2022-04-02 15:38:46 -0400

    create schema.sql for the database and test server connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
-require('.dotenv').config();
+require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  let sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  let sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS cms_tech_blog;
+CREATE DATABASE cms_tech_blog;
