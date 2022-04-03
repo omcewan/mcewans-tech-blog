da2fde09e25c2a4cab29867a7b1b65ebf0a88c63
omcewan <orlandomcewan231@gmail.com>
2022-04-03 13:41:53 -0400

    install express-handlebars and set it up as the template engine in server.js

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+// import packages and set variables
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./controllers')
-
 const path = require('path');
-
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
 const sequelize = require('./config/connection');
 
+// set up template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// express middleware functions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes)
 
+// turn on routes
+app.use(routes)
 
 // turn on the connection to db and server
 sequelize.sync({ force: false }).then(() => {
