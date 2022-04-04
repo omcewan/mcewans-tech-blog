7f704038f9e7f0f95dea83d749d91799e4b260d7
omcewan <orlandomcewan231@gmail.com>
2022-04-03 21:38:02 -0400

    create stylesheet and populate homepage with posts using handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,8 +8,7 @@ router.get('/', (req, res) => {
     include: { model: User, attributes: ['username']},
   }).then(postData => {
     const posts = postData.map(post => post.get({plain: true}))
-    console.log(posts)
-    res.render('homepage');
+    res.render('homepage', {posts});
   })
 });
 
