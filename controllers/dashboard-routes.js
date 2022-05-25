211b2f235f3f58688081f6c73b0429f554933463
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-05-25 00:21:33 -0400

    display user posts on dasboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,8 +3,21 @@ const sequlize = require('../config/connection');
 const { User, Post } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('dashboard', {loggedIn: true})
-})
+  Post.findAll({
+    // use the ID from the session
+    where: { id: req.session.user_id },
+    attributes: ['id', 'title', 'post_contents', 'createdAt'],
+    include: { model: User, attributes: ['username'] },
+  })
+    .then((userData) => {
+      console.log(userData);
+      const posts = userData.map((post) => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-
-module.exports = router
+module.exports = router;
