6181eb0cacfdb93fe08eb5e432e8f51ef3b845f2
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-05-28 00:57:16 -0400

    set up single post page. clicking on post takes user to page where post is displayed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,4 +20,31 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ['id', 'title', 'post_contents', 'createdAt'],
+    include: { model: User, attributes: ['username'] },
+  })
+    .then((postData) => {
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = postData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
