b4d0464440e5e076ca37a2e0e5bcfa71fe57f11d
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-06-05 18:30:07 -0400

    updated edit-post.js to have a button to go back to the dashboard

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,15 @@
+const dayjs = require('dayjs');
+const localizedFormat = require('dayjs/plugin/localizedFormat');
+dayjs.extend(localizedFormat);
+
 module.exports = {
   format_username: (username) => {
     const updated_usename = username.split('');
     updated_usename[0] = updated_usename[0].toUpperCase();
     return updated_usename.join('');
+  },
+
+  formatDate: (createdAtVal) => {
+    return dayjs(createdAtVal).format('lll');
   },
 };
