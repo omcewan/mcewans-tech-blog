537057d92cfe8fdc5b8bbce1aeb5664e2571d4aa
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-06-06 18:14:49 -0400

    add Comment model and associations to Post and User Models

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,7 @@ const localizedFormat = require('dayjs/plugin/localizedFormat');
 dayjs.extend(localizedFormat);
 
 module.exports = {
-  format_username: (username) => {
+  format_username: (username= "your" ) => {
     const updated_usename = username.split('');
     updated_usename[0] = updated_usename[0].toUpperCase();
     return updated_usename.join('');
